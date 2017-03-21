32bdccc6d288b3c5799a972fae9cd1d153691ca4
Veev <veev520@sina.com>
2017-03-21 22:33:16 +0800

    Initial commit

---
+++ src/club/veev/hello/server/Log.java
@@ -0,0 +1,11 @@
+package club.veev.hello.server;
+
+/**
+ * Created by Veev on 2017/3/21
+ */
+public class Log {
+
+    public static void i(Object obj) {
+        System.out.print(obj.toString());
+    }
+}
---
+++ src/club/veev/hello/server/Main.java
@@ -0,0 +1,11 @@
+package club.veev.hello.server;
+
+/**
+ * Created by Veev on 2017/3/21
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        Log.i("Hello World!");
+    }
+}
